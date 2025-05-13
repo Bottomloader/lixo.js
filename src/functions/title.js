5b0da2e4cdb41fc19922c6465851ad2c0c2554a5
Bottomloader <imsamlolll@gmail.com>
2025-05-13 11:27:35 +0300

    New functions

--- src/functions/title.js
+++ src/functions/title.js
@@ -1,11 +1,12 @@
-module.exports = {
-    name: "$title",
-    description: "Sets the title of the embed.",
-    takesBrackets: true,
-    execute: async (context, args) => {
-        if (!args[0]) return "Error: $title requires text";
-        context.embedData = context.embedData || {};
-        context.embedData.title = args[0];
-        return "";
-    }
+module.exports = {  
+    name: "$title",  
+    description: "Sets the title of the embed. Args: [text];[url]",  
+    takesBrackets: true,  
+    execute: async (context, args) => {  
+        if (!args[0]) return "Error: $title requires text";  
+        context.embedData = context.embedData || {};  
+        context.embedData.title = args[0];  
+        if (args[1]) context.embedData.url = args[1];
+        return "";  
+    }  
 };
