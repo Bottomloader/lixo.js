0fd90c53d14d8c0c11d0d94db14f3864f1f46865
Bottomloader <imsamlolll@gmail.com>
2025-05-15 00:50:15 +0300

    Fix ofc

--- src/functions/cropText.js
+++ src/functions/cropText.js
@@ -4,7 +4,7 @@ module.exports = {
     takesBrackets: true,
     execute: async (context, args) => {
         if (!args[0] || args[1] === undefined) return "Error: $cropText requires text and maxLength";
-        if (isNaN(args[1]) return "Error: $cropText expected a number"
+        if (isNaN(args[1])) return "Error: $cropText expected a number"
         const number = parseInt(args[1], 10);
         const elipsis = args[2] == undefined ? "" : args[2];
         return args[0].slice(0, number) + elipsis;
