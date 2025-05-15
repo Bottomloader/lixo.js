725da209d798e17c45be8cb2a10d7c0d6710e97d
Bottomloader <imsamlolll@gmail.com>
2025-05-15 17:53:38 +0300

    Fix and new thing

--- src/functions/channelID.js
+++ src/functions/channelID.js
@@ -1,9 +1,19 @@
 module.exports = {
-    name: "$c",
-    description: "Ignores the content within the brackets. Used for adding comments to code.",
+    name: "$channelID",
+    description: "Returns the current channel ID.",
     takesBrackets: true,
     execute: async (context, args) => {
-        if (context.channel) return context.channel.id;
+        if (args[0]) {
+          try {
+            const channelId =  args[0];
+            targetChannel = context.client.channels.cache.get(channelId) || await context.client.channels.fetch(channelId);
+            return targetChannel.name;
+          } catch (e) {
+            return "Error: $channelID couldn't fetch the channel with the reason:" + e;
+          }
+        } else if (context.channel) {
+          return context.channel.id;
+        };
         return "Error: $channelID couldn't find the channel.";
     }
 };
