6a6b02b8e4f448032b7896bb81163e30b9afabfb
Ivaylo <imsamlolll@gmail.com>
2025-05-20 18:21:40 +0300

    I am too dumb for this world

--- src/functions/startContainer.js
+++ src/functions/startContainer.js
@@ -7,7 +7,7 @@ module.exports = {
     execute: async (context, args) => {
         context.components = context.components || [];
         context.checks = context.checks || {};
-        if (context.checks.compv2? === true) return console.warn("Error: cannot create container inside of a continer");
+        if (context.checks?.compv2 === true) return console.warn("Error: cannot create container inside of a continer");
         context.checks.compv2 = true;
         if (context.checks.compv2 == true) return "Error: cannot create container inside of a continer"
         const payload = {};
